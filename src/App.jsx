528fc93ee94d30cfa2be05f81ddf68ea9d1b825b
Antoine L <162304112+t0rbenC0rtes@users.noreply.github.com>
2025-11-01 21:33:31 +0100

    Update voting button text and re-enable functionality for approved stories

--- src/App.jsx
+++ src/App.jsx
@@ -342,15 +342,15 @@ function App() {
 										</button>
 										<button
 											className="main-button voting-button"
-											// onClick={() =>
-											// 	setCurrentView("voting")
-											// }
-											// disabled={
-											// 	approvedStories.length === 0
-											// }
+											onClick={() =>
+												setCurrentView("voting")
+											}
+											disabled={
+												approvedStories.length === 0
+											}
 										>
 											<span className="button-text">
-												{/* Voter & Deviner */}Les votes ne sont pas encore ouverts.
+												Voter & Deviner
 											</span>
 											{votingProgress && (
 												<span
